df8cbb6de6a4c53de3618bd60911e19d699cd231
Dheeraj Poonia <dheerajbishnoi123@gmail.com>
2026-02-28 20:57:25 +0530

    question

---
+++ src/main/java/org/example/module_5_collection_framework/generic_programming/questions/Question1.java
@@ -0,0 +1,18 @@
+package org.example.module_5_collection_framework.generic_programming.questions;
+
+public class Question1 {
+    public static void main(String[] args) {
+        Question1 obj = new Question1();
+        obj.printValue(10);
+        obj.printValue("Hello World");
+    }
+
+    // write generic method printValue(T value)
+    public <T> void printValue(T value) {
+        System.out.println(value);
+    }
+
+    public <T> T getValue(T value) {
+        return value;
+    }
+}
